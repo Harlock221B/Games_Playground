d451f72bc20df7eea8dd06315aaba9ca9eb00265
Gabriel Ferreira <64704484+Harlock221B@users.noreply.github.com>
2021-10-01 09:38:18 -0300

    make it better

--- games/settings.py
+++ games/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x%5+aoql_9tizj0g#w40zw2h%c31*+9opd0)c*e$hogs!r7gv_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://games-playground.herokuapp.com']
 
 
 # Application definition
